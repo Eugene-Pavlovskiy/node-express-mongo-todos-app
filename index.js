4ec84d007829d8a4665460fab33540e56e322764
Eugene Pavlovskiy <infopavlovskiy@gmail.com>
2022-01-07 22:17:26 +0200

    commit 2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const mongoose = require('mongoose')
-// const path = require('path')
+const path = require('path')
 const exphbs = require('express-handlebars')
-// const todoRoutes = require('./routes/todos')
+const todoRoutes = require('./routes/todos')
 
 const PORT = process.env.PORT || 3000
 
@@ -16,20 +16,14 @@ app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
-// app.use(express.urlencoded({ extended: true }))
-// app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
 
-// app.use(todoRoutes)
+app.use(todoRoutes)
 
 async function start() {
   try {
-    await mongoose.connect(
-      'mongodb+srv://jamalrom:1q2w3e4r@cluster0.oqpnz.mongodb.net/todos',
-      {
-        useNewUrlParser: true,
-        useFindAndModify: false
-      }
-    )
+    await mongoose.connect('mongodb+srv://jamalrom:1q2w3e4r@cluster0.oqpnz.mongodb.net/todos')
     app.listen(PORT, () => {
       console.log('Server has been started...')
     })
